db15be44831e44701d3204abd83d5775cffe7012
Hiroya Kabata <h.kabata420@gmail.com>
2014-03-07 20:42:54 +0900

    all test

--- orange_tree_spec.rb
+++ orange_tree_spec.rb
@@ -19,12 +19,12 @@ describe OrangeTree do
     @tree = OrangeTree.new
   end
 
-  it "一年経過" do
+  it "毎年一つ歳をとる" do
     @tree.oneYearPasses
     @tree.instance_variable_get(:@year).should == 1
   end
 
-  it "七年で死んでメッセージ表示" do
+  it "七歳で死んでメッセージ表示" do
     6.times do
       @tree.oneYearPasses
     end
@@ -32,5 +32,49 @@ describe OrangeTree do
       lambda{@tree.oneYearPasses}.should raise_error(SystemExit)
 	end
     output.should be_include '木は死にました'
+  end
+
+  it "三年目から毎年年齢と同じ数の実をつける" do
+    3.times do
+      @tree.instance_variable_get(:@orangeCount).should == 0
+      @tree.oneYearPasses
+    end
+	@orange = @tree.instance_variable_get(:@orangeCount) 
+    @year = @tree.instance_variable_get(:@year)
+	@orange.should == @year
+  end
+
+  it "木になっているオレンジの数を返す" do
+    6.times do
+      @tree.oneYearPasses
+    end
+	  orange = @tree.instance_variable_get(:@orangeCount)
+	  @tree.countTheOranges.should == orange 
+  end
+
+  it "オレンジがあるときに摘むとオレンジの数が一つ減りオレンジの評価を表示" do
+    6.times do
+      @tree.oneYearPasses
+    end
+	count = @tree.instance_variable_get(:@orangeCount)
+	['最高！','うまい', 'なかなか', '微妙', 'まずい'].should include capture(:stdout){@tree.pichAnOrange}.chomp
+	@tree.instance_variable_get(:@orangeCount).should == count - 1
+  end
+
+  it "オレンジがないときに摘むとオレンジがないメッセージ表示" do
+    capture(:stdout){@tree.pichAnOrange}.should be_include 'もうオレンジはありません'
+  end
+
+  it "ある年に取り残したオレンジは次の年に落ちる" do
+  # 毎年、年齢個の実がなって年齢-1個の実が落ちるので、1個ずつ増える
+    4.times do
+      @tree.oneYearPasses
+    end
+    2.times do
+      @tree.oneYearPasses
+      count = @tree.instance_variable_get(:@orangeCount)
+	  year = @tree.instance_variable_get(:@year)
+	  count.should == year - 3
+    end
   end
 end
